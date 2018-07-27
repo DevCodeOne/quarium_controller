f8c1a4db44fe43b2e645a117ae18cf35076aa19a
DevCodeOne <christian.r.dev@googlemail.com>
2018-07-27 23:28:43 +0200

    Added the rest of the required dependencies

--- src/quarium_controller.cpp
+++ src/quarium_controller.cpp
@@ -1,8 +1,10 @@
 #include "config.h"
 
 #include "pistache/endpoint.h"
+#include "network.h"
 
 int main(int argc, char *argv[]) {
     config::instance();
+    network::wpa_interface_path interface_path("/var/run/wpa_supplicant", "wlan0pistachste");
     return EXIT_SUCCESS;
 }
