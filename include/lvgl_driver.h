f25bb6a847032897785ff50587408c97573f9132
DevCodeOne <christian.r.dev@googlemail.com>
2018-08-11 00:06:44 +0200

    Added the display driver for lvgl

---
+++ include/lvgl_driver.h
@@ -0,0 +1,73 @@
+#pragma once
+
+// clang-format off
+#include <unistd.h>
+#include <limits.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/kd.h>
+#include <sys/vt.h>
+#include <linux/fb.h>
+// clang-format on
+
+#include <memory>
+#include <mutex>
+#include <chrono>
+#include <algorithm>
+
+#include "lvgl.h"
+
+class lvgl_driver {
+   public:
+    static std::shared_ptr<lvgl_driver> instance();
+
+    ~lvgl_driver();
+
+    explicit operator bool() const;
+
+   private:
+    template<typename T>
+    static void do_copy(int32_t x1, int32_t y1, int32_t x2, int32_t y2, const lv_color_t *color_pointer);
+    static void flush_buffer(int32_t x1, int32_t y1, int32_t x2, int32_t y2, const lv_color_t *color_pointer);
+
+    lvgl_driver();
+
+    fb_var_screeninfo m_variable_info;
+    fb_fix_screeninfo m_fixed_info;
+    uint64_t m_framebuffer_memory_length = 0;
+    int m_framebuffer_descriptor = 0;
+    char *m_framebuffer_memory = nullptr;
+    bool m_is_valid = true;
+
+    static inline std::shared_ptr<lvgl_driver> _instance;
+    static inline std::mutex _instance_mutex;
+};
+
+template<typename T>
+void lvgl_driver::do_copy(int32_t x1, int32_t y1, int32_t x2, int32_t y2, const lv_color_t *color_pointer) {
+    auto inst = instance();
+
+    if (inst == nullptr || inst->operator bool() == false) {
+        logger::instance()->warn("lv driver is not valid");
+        return;
+    }
+
+    int act_x1 = std::clamp<int>(x1, 0, inst->m_variable_info.xres - 1);
+    int act_y1 = std::clamp<int>(y1, 0, inst->m_variable_info.yres - 1);
+    int act_x2 = std::clamp<int>(x2, 0, inst->m_variable_info.xres - 1);
+    int act_y2 = std::clamp<int>(y2, 0, inst->m_variable_info.yres - 1);
+
+    T *pixels = (T *)inst->m_framebuffer_memory;
+
+    int32_t line_length = inst->m_fixed_info.line_length / sizeof(T);
+
+    for (int32_t y_it = act_y1; y_it <= act_y2; ++y_it) {
+        long off_y = y_it * line_length;
+        for (int32_t x_it = act_x1; x_it <= act_x2; ++x_it) {
+            pixels[(x_it + inst->m_variable_info.xoffset) + off_y] = color_pointer->full;
+            ++color_pointer;
+        }
+        color_pointer += (x2 - act_x2);
+    }
+}
---
+++ src/lvgl_driver.cpp
@@ -0,0 +1,109 @@
+#include "logger.h"
+
+#include "lvgl_driver.h"
+
+std::shared_ptr<lvgl_driver> lvgl_driver::instance() {
+    std::lock_guard<std::mutex> instance_guard{_instance_mutex};
+
+    if (!_instance) {
+        _instance = std::shared_ptr<lvgl_driver>(new lvgl_driver);
+    }
+
+    return _instance;
+}
+
+lvgl_driver::lvgl_driver() {
+    int tty_fd = open("/dev/tty0", O_RDONLY, 0);
+
+    if (tty_fd < 0) {
+        logger::instance()->warn("An error occured when trying to open /dev/tty0 {}", strerror(errno));
+    }
+
+    // if (ioctl(tty_fd, KDSETMODE, KD_TEXT) < 0) {
+    //     logger::instance()->warn("An error occured when trying to set the mode of /dev/tty0 {}", strerror(errno));
+    // }
+
+    if (ioctl(tty_fd, KDSETMODE, KD_GRAPHICS) < 0) {
+        logger::instance()->warn("An error occured when trying to set the mode of /dev/tty0 {}", strerror(errno));
+    }
+
+    // Make configurable via settings.json
+    m_framebuffer_descriptor = open("/dev/fb0", O_RDWR);
+
+    if (m_framebuffer_descriptor < 0) {
+        logger::instance()->warn("An error occured when trying to open the framebuffer /dev/fb0");
+        m_is_valid = false;
+    }
+
+    if (ioctl(m_framebuffer_descriptor, FBIOGET_VSCREENINFO, &m_variable_info) < 0) {
+        logger::instance()->warn("An error occured when trying to get the variable info of /dev/fb0");
+        m_is_valid = false;
+    }
+
+    m_variable_info.bits_per_pixel = 16;
+
+    if (ioctl(m_framebuffer_descriptor, FBIOPUT_VSCREENINFO, &m_variable_info) < 0) {
+        logger::instance()->warn("An error occured when trying to set the variable info of /dev/fb0");
+        m_is_valid = false;
+    }
+
+    if (ioctl(m_framebuffer_descriptor, FBIOGET_FSCREENINFO, &m_fixed_info) < 0) {
+        logger::instance()->warn("An error occured when trying to get the fixed info of /dev/fb0");
+        m_is_valid = false;
+    }
+
+    m_framebuffer_memory_length = m_variable_info.xres * m_variable_info.yres * m_variable_info.bits_per_pixel / 8;
+
+    m_framebuffer_memory = (char *)mmap(nullptr, m_framebuffer_memory_length, PROT_READ | PROT_WRITE, MAP_SHARED,
+                                        m_framebuffer_descriptor, 0);
+
+    if ((void *)m_framebuffer_memory == MAP_FAILED) {
+        logger::instance()->warn("Couldn't mmap framebuffer memory");
+        m_is_valid = false;
+    }
+}
+
+lvgl_driver::~lvgl_driver() {
+    int tty_fd = open("/dev/tty0", O_RDONLY, 0);
+
+    if (tty_fd < 0) {
+        logger::instance()->warn("An error occured when trying to open /dev/tty0 {}", strerror(errno));
+    }
+    if (ioctl(tty_fd, KDSETMODE, KD_TEXT) < 0) {
+        logger::instance()->warn("An error occured when trying to set the mode of /dev/tty0 {}", strerror(errno));
+    }
+}
+
+void lvgl_driver::flush_buffer(int32_t x1, int32_t y1, int32_t x2, int32_t y2, const lv_color_t *color_pointer) {
+    auto inst = instance();
+
+    if (inst == nullptr || inst->operator bool() == false) {
+        logger::instance()->warn("lv driver is not valid");
+        return;
+    }
+
+    if (inst->m_framebuffer_memory == nullptr || x2 < 0 || y2 < 0 || x1 > inst->m_variable_info.xres - 1 ||
+        y1 > inst->m_variable_info.yres - 1) {
+        lv_flush_ready();
+        return;
+    }
+
+    switch (inst->m_variable_info.bits_per_pixel) {
+        case 32:
+        case 24:
+            do_copy<uint32_t>(x1, y1, x2, y2, color_pointer);
+            break;
+        case 16:
+            do_copy<uint16_t>(x1, y1, x2, y2, color_pointer);
+            break;
+        case 8:
+            do_copy<uint8_t>(x1, y1, x2, y2, color_pointer);
+            break;
+        default:
+            break;
+    }
+
+    lv_flush_ready();
+}
+
+lvgl_driver::operator bool() const { return m_is_valid; }
