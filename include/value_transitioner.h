66a4121ac9696f07e5b7f339f39715c8449072da
DevCodeOne <christian.r.dev@googlemail.com>
2019-10-27 15:00:14 +0100

    Improved value_transitioner a bit

--- include/value_transitioner.h
+++ include/value_transitioner.h
@@ -6,43 +6,55 @@
 #include <mutex>
 #include <thread>
 
+// TODO: make at least moveable
 template<typename ValueType>
 class value_transitioner final {
    public:
     using value_type_t = ValueType;
 
-    template<typename Callable>
-    value_transitioner(Callable transition_step, value_type_t current_value,
-                       std::chrono::milliseconds period = std::chrono::milliseconds(100));
-    value_transitioner(value_transitioner &other);
-    value_transitioner(const value_transitioner &&other);
+    value_transitioner(value_type_t current_value);
+    value_transitioner(const value_transitioner &other) = delete;
+    value_transitioner(value_transitioner &&other) = delete;
     ~value_transitioner();
 
-    value_transitioner &operator=(value_transitioner &other);
-    value_transitioner &operator=(const value_transitioner &&other);
+    value_transitioner &operator=(const value_transitioner &other) = delete;
+    value_transitioner &operator=(value_transitioner &&other) = delete;
+
+    template<typename Callable>
+    value_transitioner &start_transition_thread(Callable callable,
+                                                std::chrono::milliseconds period = std::chrono::milliseconds(100));
 
     value_transitioner &target_value(value_type_t new_target);
+    value_type_t current_value() const;
 
    private:
     template<typename Callable>
     void do_transitions(Callable transition_step);
 
-    mutable std::mutex m_intance_mutex;
+    mutable std::mutex m_instance_mutex;
+    mutable std::recursive_mutex m_value_mutex;
     std::thread m_transition_thread;
     std::condition_variable m_wake_up;
-    std::chrono::milliseconds m_period;
+    std::chrono::milliseconds m_period = std::chrono::milliseconds(100);
     std::atomic_bool m_exit_thread;
+    std::atomic_bool m_is_running;
 
     value_type_t m_target_value;
     value_type_t m_current_value;
 };
 
 template<typename ValueType>
+value_transitioner<ValueType>::value_transitioner(value_type_t current_value)
+    : m_current_value(current_value), m_target_value(current_value), m_exit_thread(false) {}
+
+template<typename ValueType>
 template<typename Callable>
-value_transitioner<ValueType>::value_transitioner(Callable transition_step, value_type_t current_value,
-                                                  std::chrono::milliseconds period)
-    : m_current_value(current_value), m_target_value(current_value), m_exit_thread(false), m_period(period) {
-    m_transition_thread = std::thread([this, transition_step]() { do_transitions(transition_step); });
+auto value_transitioner<ValueType>::start_transition_thread(Callable callable, std::chrono::milliseconds period)
+    -> value_transitioner & {
+    std::lock_guard<std::mutex> instance_guard{m_instance_mutex};
+
+    m_transition_thread = std::thread([this, callable]() { do_transitions(callable); });
+    return *this;
 }
 
 template<typename ValueType>
@@ -57,10 +69,17 @@ value_transitioner<ValueType>::~value_transitioner() {
 
 template<typename ValueType>
 auto value_transitioner<ValueType>::target_value(value_type_t target_value) -> value_transitioner<value_type_t> & {
-    std::lock_guard<std::mutex> instance_guard{m_intance_mutex};
+    std::lock_guard<std::mutex> instance_guard{m_instance_mutex};
 
     m_target_value = target_value;
     return *this;
+}
+
+template<typename ValueType>
+auto value_transitioner<ValueType>::current_value() const -> value_type_t {
+    std::lock_guard<std::recursive_mutex> value_guard{m_value_mutex};
+
+    return m_current_value;
 }
 
 template<typename ValueType>
@@ -72,14 +91,17 @@ void value_transitioner<ValueType>::do_transitions(Callable transition_step) {
     milliseconds delta_time{0};
 
     while (!m_exit_thread.load()) {
-        std::unique_lock<std::mutex> instance_guard{m_intance_mutex};
+        std::unique_lock<std::mutex> instance_guard{m_instance_mutex};
 
         delta_time = duration_cast<milliseconds>(steady_clock::now().time_since_epoch()) - then;
 
         auto time_transition_step_call = steady_clock::now();
 
         // Maybe check return value, to check if the transition is already done
-        transition_step(delta_time, m_current_value, m_target_value);
+        {
+            std::lock_guard<std::recursive_mutex> value_guard{m_value_mutex};
+            transition_step(delta_time, m_current_value, m_target_value);
+        }
 
         then = duration_cast<milliseconds>(steady_clock::now().time_since_epoch());
         m_wake_up.wait_until(instance_guard, time_transition_step_call + m_period);
--- tests/value_transitioner_test.cpp
+++ tests/value_transitioner_test.cpp
@@ -7,33 +7,45 @@
 
 TEST_CASE("value_transitioner basic test") {
     int value = 0;
+    int index = 0;
 
-    auto transition_step = [](auto time_diff, auto &current_value, const auto &target_value) -> bool {
-        if (*current_value == *target_value) {
+    value_transitioner<int> transitioner(value);
+    auto update_local_values = [&value, &transitioner, &index]() {
+        value = transitioner.current_value();
+        std::cout << value << std::endl;
+        ++index;
+    };
+    auto transition_step = [&update_local_values](auto time_diff, auto &current_value,
+                                                  const auto &target_value) -> bool {
+        std::cout << "Running" << std::endl;
+        if (current_value == target_value) {
             return false;
         }
 
-        int distance = *target_value - *current_value;
+        int distance = target_value - current_value;
 
         if (std::abs(distance) > 10) {
             int one_step = (int)(distance / std::abs(distance) * 10);
-            *current_value += one_step;
+            current_value += one_step;
         } else {
-            *current_value = *target_value;
+            current_value = target_value;
         }
+
+        update_local_values();
 
         return true;
     };
 
-    value_transitioner<int *> transitioner(transition_step, &value);
+    transitioner.start_transition_thread(transition_step);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
     int target = 100;
 
-    transitioner.target_value(&target);
+    transitioner.target_value(target);
 
-    // TODO: improve this test
+    // TODO: improve this test has race condition, though not a very critical one
     std::this_thread::sleep_for(std::chrono::milliseconds((10 + 1) * 100));
     REQUIRE(value == target);
+    REQUIRE(index == 10);
 }
